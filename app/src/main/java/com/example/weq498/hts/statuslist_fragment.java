aac97792cda5efecf17ec2d9f8ab5662cfcd1dc6
吳包子 <weq498aa@gmail.com>
2015-08-29 04:43:19 +0800

    8/29 4:42 更新 待改

--- app/src/main/java/com/example/weq498/hts/statuslist_fragment.java
+++ app/src/main/java/com/example/weq498/hts/statuslist_fragment.java
@@ -6,12 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by User on 2015/8/29.
  */
 public class statuslist_fragment extends Fragment {
-
+    private ListView showlist;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,9 @@ public class statuslist_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.statuslist_fragment, container, false);
+        showlist = (ListView) v.findViewById(R.id.showlist);
+        String[] array = {"使用者狀態","更改密碼"};
+        //showlist.setAdapter(new MyAdapter());
         //white something
         return v;
     }
