127226f63ed756aff36739661327f38df88a7844
吳包子 <weq498aa@gmail.com>
2015-08-29 02:59:38 +0800

    add statuslist_fragment

--- app/src/main/java/com/example/weq498/hts/login_menu.java
+++ app/src/main/java/com/example/weq498/hts/login_menu.java
@@ -1,7 +1,24 @@
 package com.example.weq498.hts;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by User on 2015/8/28.
  */
-public class login_menu {
+public class login_menu extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_menu);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+    }
 }
---
+++ app/src/main/java/com/example/weq498/hts/statuslist_fragment.java
@@ -0,0 +1,26 @@
+package com.example.weq498.hts;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by User on 2015/8/29.
+ */
+public class statuslist_fragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.statuslist_fragment, container, false);
+        //white something
+        return v;
+    }
+}
